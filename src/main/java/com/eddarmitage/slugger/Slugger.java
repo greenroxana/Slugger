7d7a8b621ebb87a658e3b92b11d5d8dbc60710c5
Edd Armitage <edward.armitage@gmail.com>
2016-02-07 18:30:50 +0000

    Allow case to be preserved

--- src/main/java/com/eddarmitage/slugger/Slugger.java
+++ src/main/java/com/eddarmitage/slugger/Slugger.java
@@ -11,65 +11,61 @@ public class Slugger {
     private static final String NOT_SLUG_CHARS_REGEX = "\\W";
     private static final String DEFAULT_SEPARATOR = "-";
 
+    private final WordSplitter wordSplitter;
+
     private final OptionalInt targetLength;
     private final boolean enforceHardLimit;
     private final CharSequence separator;
-    private final WordSplitter wordSplitter;
+    private final boolean preserveCase;
+    private final Locale locale;
 
 
     /**
      * Creates a {@code Slugger} with the given configuration.
      *
-     * @param targetLength the target length that most slugs will not normally be longer than.
+     * @param targetLength     the target length that most slugs will not normally be longer than.
      * @param enforceHardLimit force the slugs to be no longer than the target length
-     * @param separator the character sequence used to join consecutive words in the slug
+     * @param separator        the character sequence used to join consecutive words in the slug
      */
-    private Slugger(WordSplitter wordSplitter, OptionalInt targetLength, boolean enforceHardLimit, CharSequence separator) {
+    private Slugger(WordSplitter wordSplitter, OptionalInt targetLength, boolean enforceHardLimit, boolean preserveCase, CharSequence separator) {
         this.wordSplitter = wordSplitter;
         this.targetLength = targetLength;
         this.enforceHardLimit = enforceHardLimit;
+        this.preserveCase = preserveCase;
+        this.locale = Locale.getDefault();
         this.separator = separator;
     }
 
     public static Slugger create() {
-        return new Slugger(WordSplitters.whitespaceWordSplitter(), OptionalInt.empty(), false, DEFAULT_SEPARATOR);
+        return new Slugger(WordSplitters.whitespaceWordSplitter(), OptionalInt.empty(), false, false, DEFAULT_SEPARATOR);
     }
 
     public Slugger withTargetLength(final int targetLength) {
-        return new Slugger(wordSplitter, OptionalInt.of(targetLength), enforceHardLimit, separator);
+        return new Slugger(wordSplitter, OptionalInt.of(targetLength), enforceHardLimit, preserveCase, separator);
     }
 
     public Slugger withSeparator(CharSequence separator) {
-        return new Slugger(wordSplitter, targetLength, enforceHardLimit, separator);
+        return new Slugger(wordSplitter, targetLength, enforceHardLimit, preserveCase, separator);
     }
 
     public Slugger withHardLimitsEnforced() {
-        return new Slugger(wordSplitter, targetLength, true, separator);
+        return new Slugger(wordSplitter, targetLength, true, preserveCase, separator);
     }
 
     public Slugger withWordSplitter(WordSplitter wordSplitter) {
-        return new Slugger(wordSplitter, targetLength, enforceHardLimit, separator);
+        return new Slugger(wordSplitter, targetLength, enforceHardLimit, preserveCase, separator);
     }
 
     public Slugger withAdditionalWordSplitter(WordSplitter additionalWordSplitter) {
-        return new Slugger(wordSplitter.withAdditionalSplitter(additionalWordSplitter), targetLength, enforceHardLimit, separator);
+        return new Slugger(wordSplitter.withAdditionalSplitter(additionalWordSplitter), targetLength, enforceHardLimit, preserveCase, separator);
+    }
+
+    public Slugger withCasePreserved() {
+        return new Slugger(wordSplitter, targetLength, enforceHardLimit, true, separator);
     }
 
     /**
      * Generates a slug-version of the provided input.
-     *
-     * <p>See the following samples of slugs generated with a {@code targetLength}
-     * of 25 characters, as taken from the Spock Specification:
-     *
-     * <table summary="Sample inputs and outputs">
-     *     <tr><th>Input</th><th>Output</th></tr>
-     *     <tr><td>{@code "post"}</td><td>{@code "post"}</td></tr>
-     *     <tr><td>{@code "POST!"}</td><td>{@code "post"}</td></tr>
-     *     <tr><td>{@code "postTitle"}</td><td>{@code "post-title"}</td></tr>
-     *     <tr><td>{@code "post_title"}</td><td>{@code "post-title"}</td></tr>
-     *     <tr><td>{@code "This is a very long title for a post"}</td>
-     *          <td>{@code "this-is-a-very-long-title"}</td></tr>
-     * </table>
      *
      * @param input the input string to be turned into a slug
      * @return The URL-safe slug
@@ -80,13 +76,17 @@ public class Slugger {
         }
 
         return wordSplitter.splitWords(input.trim())
-                .map(Slugger::sluggifyWord)
+                .map(this::replaceNonSlugCharacters)
+                .map(this::changeCase)
                 .collect(SlugBuilder.collector(targetLength, enforceHardLimit, separator));
     }
 
-    private static String sluggifyWord(String word) {
+    private String replaceNonSlugCharacters(String word) {
         return Normalizer.normalize(word, Form.NFC)
-                .replaceAll(NOT_SLUG_CHARS_REGEX, "")
-                .toLowerCase(Locale.ENGLISH);
+                .replaceAll(NOT_SLUG_CHARS_REGEX, "");
+    }
+
+    private String changeCase(String word) {
+        return preserveCase ? word : word.toLowerCase(locale);
     }
 }
--- src/test/resources/com/eddarmitage/slugger/bdd/character-replacing.feature
+++ src/test/resources/com/eddarmitage/slugger/bdd/character-replacing.feature
@@ -4,3 +4,9 @@ Feature: Remove characters in slugs, replacing them where appropriate
   Scenario: Special characters should be removed from inputs
     When the input string "test input $%.@€/]{" is sluggified
     Then the output will contain a '-' character
+
+  Scenario: Case can be preserved in the output
+    Given case will be preserved
+    When the input string "Hello World" is sluggified
+    Then the output will contain a 'H' character
+    And the output will contain a 'W' character
