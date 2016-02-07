38cc263e0ef352867e5ef54b94c7b809707290b1
Edd Armitage <edward.armitage@gmail.com>
2016-02-07 14:50:03 +0000

    docs(README): expand README and write unit-tests for examples included

--- src/main/java/com/eddarmitage/slugger/SlugBuilder.java
+++ src/main/java/com/eddarmitage/slugger/SlugBuilder.java
@@ -6,11 +6,30 @@ import java.util.stream.Collector;
 
 import static java.lang.Integer.min;
 
+/**
+ * A SlugBuilder acts like a {@link java.util.StringJoiner}, but with the
+ * ability to constrain the length of the result.
+ *
+ * If a target length has been provided, then the SlugBuilder will produce an
+ * output that is no longer than this length, except for the case when the
+ * first addition to an empty SlugBuilder would exceed this limit, and
+ * {@code enforceHardLimit} is set to False.
+ *
+ * If {@code enforceHardLimit} is enabled, then even in this case the initial
+ * word added to the SlugBuilder will be truncated, such that the output is no
+ * longer than the target length.
+ */
 class SlugBuilder {
 
     private final OptionalInt targetLength;
     private final boolean enforceHardLimit;
     private final StringJoiner joiner;
+
+    SlugBuilder(CharSequence separator) {
+        this.targetLength = OptionalInt.empty();
+        this.enforceHardLimit = false;
+        this.joiner = new StringJoiner(separator);
+    }
 
     SlugBuilder(OptionalInt targetLength, boolean enforceHardLimit, CharSequence separator) {
         this.targetLength = targetLength;
