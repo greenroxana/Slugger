86ebf636b38f049cdcf9287b1fbfd6c915807898
Edd Armitage <edward.armitage@gmail.com>
2016-02-19 21:52:19 +0000

    docs(javadoc): add basic level of javadoc
    Add Javadoc comments to all public methods and classes, and add the maven
    javadoc plugin to build it all.

--- src/main/java/com/eddarmitage/slugger/splitting/WordSplitters.java
+++ src/main/java/com/eddarmitage/slugger/splitting/WordSplitters.java
@@ -3,7 +3,11 @@ package com.eddarmitage.slugger.splitting;
 import com.eddarmitage.slugger.WordSplitter;
 
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
+/**
+ * {@code WordSplitter} implementations, as well as methods to create WordSplitters from a Regex String.
+ */
 public class WordSplitters {
 
     private static final Pattern WHITESPACE_WORD_PATTERN = Pattern.compile("\\s+");
@@ -14,22 +18,56 @@ public class WordSplitters {
         throw new AssertionError("Utility class not designed for instantiation");
     }
 
+    /**
+     * A {@code WordSplitter} instance that splits based on whitespace.
+     *
+     * @return a {@link WordSplitter} instance that splits based on whitespace
+     */
     public static WordSplitter whitespaceWordSplitter() {
         return new RegexWordSplitter(WHITESPACE_WORD_PATTERN);
     }
 
+    /**
+     * A {@code WordSplitter} instance that splits camel-case inputs.
+     *
+     * @return a {@link WordSplitter} instance that splits camel-case inputs
+     */
     public static WordSplitter camelCaseWordSplitter() {
         return new RegexWordSplitter(CAMEL_CASE_SPLIT_PATTERN);
     }
 
+    /**
+     * A {@code WordSplitter} instance that splits on underscores.
+     *
+     * @return a {@link WordSplitter} instance that splits on underscores
+     */
     public static WordSplitter underscoreWordSplitter() {
         return new RegexWordSplitter(UNDERSCORE_SPLIT_PATTERN);
     }
 
+    /**
+     * A {@code WordSplitter} that splits according to a supplied regular expression {@code String}
+     * <p>
+     * The underlying implementation splits the provided input using the
+     * {@link Pattern#splitAsStream(CharSequence)} method.
+     *
+     * @param regex  the regular expression that this splitter will be based on
+     * @return a {@link WordSplitter} instance that splits according to the supplied regex
+     * @throws PatternSyntaxException if the regex's syntax is invalid
+     */
     public static WordSplitter withRegex(String regex) {
         return new RegexWordSplitter(Pattern.compile(regex));
     }
 
+    /**
+     * A {@code WordSplitter} that splits according to a supplied regular expression {@code String}
+     * <p>
+     * The underlying implementation splits the provided input using the
+     * {@link Pattern#splitAsStream(CharSequence)} method.
+     *
+     * @param regex  the compiled regular expression pattern that this splitter will be based on
+     * @return a {@link WordSplitter} instance that splits according to the supplied {@link Pattern}
+     */
     public static WordSplitter withRegex(Pattern regex) {
         return new RegexWordSplitter(regex);
     }
