9ef6b6145bd5ec4cc7d3347d85ff55aa9f56325c
Edd Armitage <edward.armitage@gmail.com>
2016-02-28 10:45:54 +0000

    Split Cucumber Steps into multiple files

---
+++ src/test/java/com/eddarmitage/slugger/bdd/SluggerHelper.java
@@ -0,0 +1,29 @@
+package com.eddarmitage.slugger.bdd;
+
+import com.eddarmitage.slugger.Slugger;
+
+public class SluggerHelper {
+
+    private Slugger slugger;
+    private String output = "";
+
+    public SluggerHelper() {
+        slugger = Slugger.create();
+    }
+
+    public Slugger get() {
+        return slugger;
+    }
+
+    public void set(Slugger slugger) {
+        this.slugger = slugger;
+    }
+
+    public void applyInput(String input) {
+        output = slugger.sluggify(input);
+    }
+
+    public String getOutput() {
+        return output;
+    }
+}
